e849763aaf8858d0a4bdcca8ea66002fc2fe2efa
Cass <cassianoleaoramos@gmail.com>
2024-11-25 22:09:15 -0300

    create basic view for Villa page

---
+++ WhiteLagoon.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WhiteLagoon.Domain.Entities;
+
+namespace WhiteLagoon.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Villa> Villas { get; set; }
+    }
+
+}
